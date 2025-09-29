f5de99f49563b6ea0798cdd568e9bf125b4c9675
ZBetsch <betschzt@mail.uc.edu>
2025-09-28 23:17:58 -0400

    Initial Commit

---
+++ src/main/java/com/zblab01/betschlab04/BetschLab04Application.java
@@ -0,0 +1,13 @@
+package com.zblab01.betschlab04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetschLab04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetschLab04Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zblab01/betschlab04/BetschLab04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zblab01.betschlab04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetschLab04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
